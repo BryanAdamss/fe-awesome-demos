d5e263fbe811cd0b462ac88dd176adf7e8ca81f1
BryanAdamss <BryanAdamss@foxmail.com>
2018-04-24 12:25:06 +0800

    2018-0424-1224-学习
    2018-0424-1224-学习总结

--- Vue/vue-music/src/api/recommend.js
+++ Vue/vue-music/src/api/recommend.js
@@ -1,5 +1,5 @@
 import jsonp from 'common/js/jsonp'
-import {commonParams, options} from './config'
+import { commonParams, options } from './config'
 import axios from 'axios'
 
 export function getRecommend() {
@@ -55,4 +55,3 @@ export function getSongList(disstid) {
     return Promise.resolve(res.data)
   })
 }
-
